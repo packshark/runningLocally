84c709932391a42213f704301f7d8ed888d9f505
packshark <sb016n@att.com>
2024-08-28 11:36:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import about, login, admin, basicUser, cust, home
+import login, admin, basicUser, customer_portal, home, operations_portal, logout
 
 page_title = "Portal"
+
+if 'loggedOut' not in st.session_state:
+    st.session_state.loggedOut = True
+if 'username' not in st.session_state:
+    st.session_state.username = ""
+if 'user_type' not in st.session_state:
+    st.session_state.user_type = ""
 
 class MultiApp:
     def __init__(self):
@@ -15,11 +22,27 @@ class MultiApp:
         })
 
     def run(self):
+         # Initialize session state if not already done
+
         with st.sidebar:
-            app = option_menu(
+            # Define different menus for different user types
+            if st.session_state.user_type == "Admin":
+                options = ['Home', 'Operations Portal','Customer Portal', 'Logout']
+                icons = ['house-fill', 'shield-fill', 'person-circle','power']
+            elif st.session_state.user_type == "Customer":
+                options = ['Home', 'Customer Portal', 'Logout']
+                icons = ['house-fill', 'person-circle', 'power']
+            elif st.session_state.user_type == "Basic":
+                options = ['Home', 'View Stats', 'Logout']
+                icons = ['house-fill', 'person-circle', 'gear-fill']
+            else:
+                options = ['Home','Login']
+                icons = ['house-fill']
+
+            app_selection = option_menu(
                 menu_title='Portal',
-                options=['Home', 'About', 'Account'],
-                icons=['house-fill', 'chat-fill', 'person-circle'],
+                options=options,
+                icons=icons,
                 menu_icon='chat-text-fill',
                 default_index=0,
                 styles={
@@ -28,32 +51,37 @@ class MultiApp:
                     "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px"},
                     "nav-link-selected": {"background-color": "#02ab21"},
                 },
-                key="main_menu"  # Add a unique key here
+                key="main_menu"  
             )
 
-        if app == 'Home':
-            home.app()
-        elif app == 'About':        
-            about.app()
-        elif app == 'Account':
-            login.app()
-        elif app == 'Basic':
-            basicUser.app()
-        elif app == 'Admin':
-            admin.app()
-        elif app == 'Cust':
-            cust.app()
+        # if app_selection == 'Logout':
+        #     st.session_state.loggedOut = True
+        #     st.session_state.username = ""
+        #     st.session_state.user_type = ""
+        #     app_entry['title'] = "Home"
+        #     app_entry["function"]()
+        # else:
+        #     # Map the selected menu option to the corresponding app function
+        #     for app_entry in self.apps:
+        #         if app_selection == app_entry["title"]:
+        #             app_entry["function"]()
+     
+        # Map the selected menu option to the corresponding app function
+        for app_entry in self.apps:
+            if app_selection == app_entry["title"]:
+                app_entry["function"]()
 
 # Create an instance of the MultiApp class
 app = MultiApp()
 
 # Add all your applications here
 app.add_app('Home', home.app)
-app.add_app('About', about.app)
-app.add_app('Account', login.app)
-app.add_app('Basic', basicUser.app)
-app.add_app('Customer', cust.app)
+app.add_app('Customer Portal', customer_portal.app)
 app.add_app('Admin', admin.app)
+app.add_app('Operations Portal', operations_portal.app)
+app.add_app('Login', login.app)
+app.add_app('Logout', logout.app)
+app.add_app('View Stats', basicUser.app)
 
 # Run the main app
 app.run()
