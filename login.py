f45a71e04755c32f50eab7803f14b913ea1c2273
packshark <sb016n@att.com>
2024-08-28 11:36:17 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,46 +1,51 @@
 import streamlit as st
-import admin, cust, basicUser
+
+import admin, basicUser, logout
+
+if 'password' not in st.session_state:
+    st.session_state.password = ""
+
 
 def app():
-    st.title("Welcome to the Packing Portal")
-    st.write("Please log in to continue")
 
-    # Initialize session state variables
-    if 'username' not in st.session_state:
-        st.session_state.username = ''
-    if 'loggedOut' not in st.session_state:
-        st.session_state.loggedOut = True
-
+    st.title("Hello")
     if st.session_state.loggedOut:
         with st.form("login_form"):
             username = st.text_input("Username", key="login_username")  # Unique key
             password = st.text_input("Password", type="password", key="login_password")  # Unique key
-            submit_button = st.form_submit_button('Submit')  # Unique key
+            submit_button = st.form_submit_button('Submit')  
 
             if submit_button:
                 if username == "phone" and password == "hellokitty":
                     st.session_state.username = username
                     st.session_state.loggedOut = False
-                    admin.app()
+                    st.session_state.user_type = "Admin"
+                    # bug - st.experimental_rerun()  # Rerun to avoid executing the rest of the code
+                    st.success("You are logged in as " + st.session_state.user_type)
+                    st.write("Please click on Home in the navigation to refresh your access.")
 
                 elif username == "laptop" and password == "chamberofsecrets":
                     st.session_state.username = username
                     st.session_state.loggedOut = False
-                    cust.app()
+                    st.session_state.user_type = "Customer"
+                    st.success("You are logged in as " + st.session_state.user_type)
+                    st.write("Please click on Home in the navigation to refresh your access.")
 
                 elif username == "hehe" and password == "helloworld":
                     st.session_state.username = username
                     st.session_state.loggedOut = False
-                    basicUser.app()
+                    st.session_state.user_type = "Basic"
+                    st.success("You are logged in as " + st.session_state.user_type)
+                    st.write("Please click on Home in the navigation to refresh your access.")
 
                 else:
                     st.error("Incorrect username or password")
     else:
-        st.success(f"Logged in as {st.session_state.username}")
+        
         if st.session_state.username == "phone":
             admin.app()
         elif st.session_state.username == "laptop":
-            cust.app()
+            logout.app()                                                                                 
         elif st.session_state.username == "hehe":
             basicUser.app()
         else:
