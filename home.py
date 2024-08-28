c61cb93a578259a18b7e8c0ff558e4681bb8e754
packshark <sb016n@att.com>
2024-08-28 11:36:01 -0700

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
 def app():
-    st.write('Home')
+    if st.session_state.username == "phone" and st.session_state.password == "hellokitty":
+        st.title("Hello " + st.session_state.username)
+        st.write("You have access to the Customer Portal and the Operations Portal.")
+
+    elif st.session_state.username == "laptop" and st.session_state.password == "chamberofsecrets":
+        st.title("Hello " + st.session_state.username)
+        st.write("You have access to the Customer Portal.")
+
+    else:
+        st.title("Welcome")
